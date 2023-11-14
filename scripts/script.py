64452532b6437a50d3e6cbe39b3b7db5af5a963a
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2023-11-14 15:59:26 +0100

    Update script.py

--- scripts/script.py
+++ scripts/script.py
@@ -4,41 +4,31 @@ import glob
 import re
 import subprocess
 
-#vedere se il file esiste in src
-#mi recupero la variabile e incremento quella
-#altrimenti set variabile v.1.0.0 e pusho il file
-
-#prende la versione, il vecchio file e il nuovo file da parametro shell
-#es. py change_viewer.py 1.0.0
-repo_url = "https://github.com/Carraro-Riccardo/Carraro-Riccardo.github.io"
+repo_url = "https://github.com/Carraro-Riccardo/test-python-versioning"
 target_directory = "./tmp"
-branch_name = "docs"
+branch_name = "main"
 subprocess.run(["git", "clone", "--branch", branch_name, repo_url, target_directory], check=True)
 
 
-first_file  = sys.argv[1]
-second_file = sys.argv[2]
+file_path  = sys.argv[1]
+file_name   = sys.argv[2]
+str_version = sys.argv[3]
 
-file_name_only = second_file[:-4]
-search_pattern = f"{file_name_only}*.pdf"
-matching_file = glob.glob("./"+search_pattern)[0]
-
-pattern = r"_v(\d+\.\d+\.\d+)\.pdf"
-str_version = re.search(pattern, matching_file).group(1)
+file_name_only = file_name[:-4]
 
 version_components = str_version.split('.')
 x = int(version_components[0])
 y = int(version_components[1])
 z = int(version_components[2])
 
-first_file_content  = ""
-second_file_content = ""
+new_file_content  = ""
+old_file_content = ""
 
-with open(first_file) as file:
-    first_file_content = file.read()
+with open(file_path+"/"+file_name) as file:
+    new_file_content = file.read()
 
-with open(second_file) as file:
-    second_file_content = file.read()
+with open("./tmp/"+file_path+"/"+file_name) as file:
+    old_file_content = file.read()
 
 def diff_letters(first, second,x,y,z):
     if first != second:
@@ -47,16 +37,6 @@ def diff_letters(first, second,x,y,z):
         y = y + 1
     return str(x) + "." + str(y) + "." + str(z)
 
-new_version = diff_letters(first_file_content, second_file_content,x,y,z)
+new_version = diff_letters(old_file_content, new_file_content,x,y,z)
 
 print("vecchia versione:\t" + str_version + "\nnuova versione:\t\t" + new_version)
-
-os.rename(matching_file, file_name_only+"_v"+new_version+".pdf")
-
-with open(first_file, "w") as file:
-    file.write(second_file_content)
-#aggiunta step al changelog per aggiornare la variabile
-#rimuoviamo il vecchio il pdf e mettiamo il nuovo con la nuova version
-
-
-#mettere il diff in un file d'appoggio
